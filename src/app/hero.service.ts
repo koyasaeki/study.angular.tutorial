4d0986be7c754665aa2eee1ba941bb72ee3e93e6
KoyaSaeki <koyasaeki@gmail.com>
2022-08-24 17:10:45 +0900

    update: データ取得をサービス層に任せる

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,15 @@
+import type { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+
   constructor() {}
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import type { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 
-  ngOnInit(): void {}
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
