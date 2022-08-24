392942b2c8f9838539904e25a5630dd92f8e2cd4
KoyaSaeki <koyasaeki@gmail.com>
2022-08-24 16:35:48 +0900

    refactor: ヒーロー詳細を別コンポーネントにする

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import type { Hero } from '../hero';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero?: Hero;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
